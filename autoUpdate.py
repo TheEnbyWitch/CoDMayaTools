876ecd4d42bc9119bdea0b3d8a7073915bc244af
Philip <theredfoxyt@gmail.com>
2017-06-27 11:45:42 +0100

    Update autoUpdate.py

--- autoUpdate.py
+++ autoUpdate.py
@@ -1,3 +1,19 @@
+# AutoUpdate made for CoD Maya Tools by Scobalula with Aidan's code.
+# AutoUpdater is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This can be adapted to any script by passing the arguments below.
+
 import urllib2
 import socket
 import os
@@ -6,15 +22,18 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-name", help="Name of file to update relative to directory of the EXE (with ext.).", required = True, dest="name")
 parser.add_argument("-version", help="Current Version of the script.", required = True, dest="version")
 parser.add_argument("-version_info_url", help="Version info URL containing version and URL to new file on seperate lines.", required = True, dest = "version_url")
 
 results = parser.parse_args()
 
+NAMEOFSCRIPT = results.name.split(".")[0]
 FILE_VERSION = results.version
 VERSION_CHECK_URL = results.version_url
 WORKING_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
-CODMAYAPY = os.path.join(WORKING_DIR, "CoDMayaTools.py")
+PYFILE = os.path.join(WORKING_DIR, results.name)
+CLOSETIME = 2
 
 def HasInternetAccess():
     # Apparently we needed Admin privs. to use ping -c that Aidan used originally..
@@ -40,11 +59,10 @@ def CheckForUpdates():
         if not info or len(info) == 0:
             return None
 
-        CODMAYAPY = os.path.join(WORKING_DIR, "CoDMayaTools.py")
         mostRecentVersion = float(info[0])
         downloadUpdateURL = info[1] # Location of the most recent file
         
-        if mostRecentVersion > int(FILE_VERSION):
+        if mostRecentVersion > float(FILE_VERSION):
             return (mostRecentVersion, downloadUpdateURL)
     except Exception as e :
         print("<-- Exception occured in \"CheckForUpdates()\": %s.\n\n" % e)
@@ -63,7 +81,7 @@ def DownloadUpdate(downloadUpdateURL):
 
         print("<-- Downloading update from: %s\n" % str(downloadUpdateURL))
 
-        with open(CODMAYAPY, 'w') as f:
+        with open(PYFILE, 'w') as f:
             f.write(newCode)
         
         print("<-- Update complete, reload script or restart Maya to apply changes.\n\n")
@@ -71,13 +89,16 @@ def DownloadUpdate(downloadUpdateURL):
         print("<-- Exception occured in \"DownloadUpdate()\": %s.\n\n" % e)
 
 if __name__ == '__main__':
-    print("<-- COD Maya Tools Update --\n\n")
+    print("<-- %s Update\n\n" % str(NAMEOFSCRIPT))
     print("<-- Checking for updates...\n\n")
     update = CheckForUpdates()
 
     if update:
         print("<-- New update available: VERSION %s\n\n" % str(update[0]))
         DownloadUpdate(update[1])
+        CLOSETIME = 5
+    else:
+    	print("<-- No updates available.\n\n")
         
-    print("This window will close in 5 seconds")
-    time.sleep(5)
+    print("This window will close in %i seconds" % CLOSETIME)
+    time.sleep(CLOSETIME)
